00cec377a33a7a2c2979210e5c813acaae2adf02
Hughgz <minhhieu.swe@gmail.com>
2024-02-24 13:55:45 +0700

    update

--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -1,28 +1,47 @@
+import 'package:app_signchat/controller/scan_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:app_signchat/controller/scan_controller.dart'; // Import your ScanController
+import 'package:camera/camera.dart';
 
-class DetectionPage extends StatelessWidget {
-  final ScanController scanController = Get.find();
-
+class ViewCamera extends StatelessWidget {
+  const ViewCamera({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Object Detection'),
-      ),
-      body: Center(
-        child: Obx(() {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Detected Object: ${scanController.label}'),
-              SizedBox(height: 16),
-              Text('Bounding Box: x=${scanController.x}, y=${scanController.y}, w=${scanController.w}, h=${scanController.h}'),
-            ],
-          );
-        }),
+      body: GetBuilder<ScanController>(
+        init: ScanController(),
+        builder: (controller) {
+          return controller.isCameraInitialized.value
+              ? Stack(
+                children: [
+                  CameraPreview(controller.cameraController),
+                  Positioned(
+                    top: controller.y,
+                    right: controller.x,
+                    child: Container(
+                      width: 1200,
+                      height: 350,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.green, width: 4.0),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                           Container(
+                            color: Colors.white,
+                            child: Text(controller.label))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+              : const Center(
+                  child: Text("Starting Recognition"),
+                );
+        },
       ),
     );
   }
