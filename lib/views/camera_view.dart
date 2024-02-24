da6a97e2d6b2688e585045dfbc21a9180b1a236c
huynguyennovem <huynguyennovem@gmail.com>
2024-02-24 13:19:07 +0700

    fix build issue
    Signed-off-by: huynguyennovem <huynguyennovem@gmail.com>

--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -13,17 +13,15 @@ class DetectionPage extends StatelessWidget {
         title: const Text('Object Detection'),
       ),
       body: Center(
-        child: Obx(() {
-          return Column(
+        child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('Detected Object: ${scanController.label}'),
               SizedBox(height: 16),
               Text('Bounding Box: x=${scanController.x}, y=${scanController.y}, w=${scanController.w}, h=${scanController.h}'),
             ],
-          );
-        }),
-      ),
+          ),
+			),
     );
   }
 }
